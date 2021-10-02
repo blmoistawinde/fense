ce9503c16bc9108660964b59ea58180617394a31
blmoistawinde <blmoistawinde@qq.com>
2021-10-02 18:15:05 +0800

    adjust preprocess

--- fense/data.py
+++ fense/data.py
@@ -5,7 +5,14 @@ import torch
 from transformers import AutoTokenizer
 from collections import defaultdict
 
+def text_preprocess(inp):
+    if type(inp) == str:
+        return re.sub(r'[^\w\s]','', inp).lower()
+    else:
+        return [re.sub(r'[^\w\s]','', x).lower() for x in inp]
+
 def infer_preprocess(tokenizer, texts, max_len):
+    texts = text_preprocess(texts)
     batch = tokenizer(texts, truncation=True, padding='max_length', max_length=max_len)
     for k in ['input_ids', 'attention_mask', 'token_type_ids']:
         batch[k] = torch.LongTensor(batch[k])
